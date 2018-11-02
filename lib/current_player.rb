64946280d9e7ebf7be726a584b42a74249e1febd
Cihan Akman <cihanozanakman@gmail.com>
2018-11-02 15:54:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,11 @@ def turn_count(board)
   return counter
 end
 
-def current_player
-
+def current_player(board)
+  counter = turn_count(board)
+  if counter % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
 end
